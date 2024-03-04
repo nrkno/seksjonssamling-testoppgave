9e24ef57c9e675f5c7b60dfcd9e931c4c688e1f9
Maren Ytterbø <110545656+ytterbo@users.noreply.github.com>
2024-03-04 13:59:07 +0100

    added settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,19 +23,24 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    launchOptions: {
+      args: ['--start-maximized'],
+    },
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     headless: false
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'seksjonssamling',
+      use: { viewport: null },
     },
 
     // {
